daefdf58da6599d3b49409038b34ffe8ca2a0d96
rajeshsharma88 <91082296+rajeshsharma88@users.noreply.github.com>
2021-11-06 20:01:48 +0530

    Added a React Component

---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MyAmazingComponent() {
+  return (
+    <div>
+      <h1 className="section-title section-title--blue">
+        This is Amazing React Component
+      </h1>
+      <p>React is great, the sky is blue, grass is green.</p>
+    </div>
+  );
+}
+
+export default MyAmazingComponent;
